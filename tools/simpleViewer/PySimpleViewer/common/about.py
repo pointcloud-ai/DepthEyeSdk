9dcdbc8f7e4fb64d1169db2f5de1be151712a654
Adam <adam@pointcloud.com>
2019-02-19 18:21:28 +0800

    remove problem

--- tools/simpleViewer/PySimpleViewer/common/about.py
+++
@@ -1,5 +0,0 @@
-SOFTWARE_NAME = 'SimpleViewer'
-COMPANY_NAME = 'pointcloud-ai'
-VERSION_NUMBER = '0.0.1'
-LICENSE = 'No license'
-SHORT_DESCRIPTION = 'A Tool Designed for display depth eye H1 images'
